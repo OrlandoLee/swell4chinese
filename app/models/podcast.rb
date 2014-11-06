eb9c824cafbc455dbf11df99d92f75d7cb75709f
OrlandoLee <zongsheng.li@appfolio.com>
2014-11-05 18:26:01 -0800

    ol - basic version of query module

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -5,4 +5,20 @@ class Podcast < ActiveRecord::Base
     time :created_at
     time :updated_at
   end
+
+  #text should be like [{'keyword1' => 1},{{'keyword2' => 3}}]
+  def self.my_search(text)
+    temp_result = self.search do
+      text.first do |pair| #should be each
+        fulltext pair.keys.first do
+          fields :keywords => pair.values.first, :title => pair.values.first
+        end
+      end
+    end
+    result = []
+    temp_result.hits.each do |hit|
+      result << hit.result
+    end
+    result
+  end
 end
