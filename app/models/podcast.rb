89e9e905e4283a056baf7ac1436c16c2cb67f1ba
OrlandoLee <zongshengli@ucla.edu>
2014-12-08 22:44:18 -0800

    ol - finished search module and test

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -7,18 +7,39 @@ class Podcast < ActiveRecord::Base
   end
 
   #text should be like [{'keyword1' => 1},{{'keyword2' => 3}}]
-  def self.my_search(text)
-    temp_result = self.search do
-      text.first do |pair| #should be each
-        fulltext pair.keys.first do
-          fields :keywords => pair.values.first, :title => pair.values.first
-        end
+  #must be an array
+  def my_search(text)
+    temp_result = search_in_model(text)
+    format(temp_result)
+  end
+  
+private
+  def construct_search_string(text)
+    serach_string = ''
+    text.each do |pair|
+      pair.values.first.times do
+        serach_string += ' '+pair.keys.first
       end
     end
+    serach_string
+  end
+  
+  def search_in_model(text)
+    temp_result = self.class.search do
+      search_string = construct_search_string(text)
+      fulltext search_string do
+        fields :keywords, :title
+      end
+      order_by :score, :desc
+    end
+    temp_result 
+  end
+  
+  def format(temp_result)
     result = []
     temp_result.hits.each do |hit|
       result << hit.result
-    end
+    end 
     result
   end
 end
